df1b1827b17655f2f49d8ab7b8c361b38401c825
akgstudio <bumaya1024@gmail.com>
2025-01-06 11:00:31 +0800

    导入必要组件

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,27 @@
 <template>
-  <div >
-  登录
+  <div>
+    <el-form label-width="80px">
+      <el-form-item label="用户名">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">登录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
+export default {
+  methods: {
+    login() {
+      
+    }
+  }
+}
 </script>
